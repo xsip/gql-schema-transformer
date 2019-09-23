ab6598939ffc01aefdf75812c4a323d67e733c93
Kerschbaumer Stefan <kerschbaumer@lean-coders.at>
2019-09-23 06:13:31 +0200

    structure idea for reducded schemadatacollection

--- src/tranform-extensions/definitions/interfaces.ts
+++ src/tranform-extensions/definitions/interfaces.ts
@@ -11,6 +11,28 @@ export interface GqlSchemaDataCollection {
     gqlAliasDefinitions: GqlAliasDefinition[];
 }
 
+// structure idea. extends and provide to export within transformExtensionClass
+export interface GqlSchemaDataCollectionReduced {
+    names: {
+        gqlTypes: string[];
+        gqlQuerys: string[];
+        gqlMutations: string[];
+        gqlInputs: string[];
+        gqlEnums: string[];
+        gqlScalars: string[];
+        gqlAliasDefinitions: string[];
+    };
+    definitions: {
+        gqlTypes: { [index: string]: GqlObjectTypeDefinition };
+        gqlQuerys: { [index: string]: GqlFunctionTypeDefinition };
+        gqlMutations: { [index: string]: GqlFunctionTypeDefinition };
+        gqlInputs: { [index: string]: GqlObjectTypeDefinition };
+        gqlEnums: { [index: string]: GqlEnumTypeDefinition };
+        gqlScalars: string[];
+        gqlAliasDefinitions: { [index: string]: GqlAliasDefinition };
+    };
+}
+
 // @ts-ignore
 export abstract class TransformExtensionClass<T = any> extends T {
     public export(data: GqlSchemaDataCollection, exportFilePath: string) {}
